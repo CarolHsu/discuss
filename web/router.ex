21f6ec0c73928db8b468a2e77bf3bc13195104e9
shesee <sheseee@gmail.com>
2017-04-22 21:19:53 +0800

    add delete button and use resources as restful routes for topics

--- web/router.ex
+++ web/router.ex
@@ -16,11 +16,13 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
+    #get "/", TopicController, :index
+    #get "/topics/new", TopicController, :new
+    #post "/topics", TopicController, :create
+    #get "/topics/:id/edit", TopicController, :edit
+    #put "/topics/:id", TopicController, :update
+    #delete "/topics/:id", TopicController, :delete
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
